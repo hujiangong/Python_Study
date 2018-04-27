4e2f0396f4f643cc654f3950dda8341cba52e4a1
hujiangong <hujiangong@meituan.com>
2018-04-27 11:28:24 +0800

    delete all

--- python库/Tkinter/Button/asdf.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Time : 2018/3/5 16:18
-# @Author : HuJiangong
-# @File : asdf.py
-# @Version : Python 3.6.4
-# @Software: PyCharm
--- 情感分析/单句测试.py
+++
@@ -1,3 +0,0 @@
-from snownlp import SnowNLP
-s=SnowNLP(u"互相理解吧，辛苦你们啦")
-print(s.sentiments)
